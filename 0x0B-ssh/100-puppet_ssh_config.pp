33ec986d4456c8b5bf000983773af65749164f88
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2023-02-03 00:56:32 -0800

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,16 @@
-# Set the config of a ssh_file
-file_line { 'Change the main private key':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
+# Setting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'No Authenticate with passowrd':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
